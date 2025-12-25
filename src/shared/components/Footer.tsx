e98c66a9ebb468ea51fab80d5d499ab8634732be
kimsa-0605 <holykimsa05@gmail.com>
2025-12-25 11:03:33 +0700

    VLIST-88 | Create main layout with header and sidebar

--- src/shared/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-slate-800 text-white p-6 mt-10 text-center rounded-t-[40px]">
-      <p className="font-bold">© 2025 English Kids Adventure🌈</p>
-    </footer>
-  );
-};
-export default Footer;
